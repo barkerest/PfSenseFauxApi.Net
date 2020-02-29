e246993b094b0428c389aaf0cf4ab55b25040c8a
Beau Barker <beau@barkerest.com>
2020-02-29 14:22:40 -0500

    Make sure that config_get works.

--- PfSenseFauxApi.Net.Tests/ConfigGet_Should.cs
+++ PfSenseFauxApi.Net.Tests/ConfigGet_Should.cs
@@ -1,14 +1,32 @@
-﻿namespace PfSenseFauxApi.Net.Tests
+﻿using System;
+using System.Text.Json;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PfSenseFauxApi.Net.Tests
 {
     public class ConfigGet_Should
     {
         private readonly Device _dev;
+        private readonly ITestOutputHelper _output;
 
-        public ConfigGet_Should()
+        public ConfigGet_Should(ITestOutputHelper output)
         {
+            _output = output ?? throw new ArgumentNullException();
             _dev = new TestConfig().GetDevice();
         }
-        
-        
+
+        [Fact]
+        public void ReturnValue()
+        {
+            var data = _dev.ConfigGet();
+            Assert.NotNull(data);
+            var json = JsonSerializer.Serialize(data, new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            });
+            
+            _output.WriteLine(json);
+        }
     }
 }
